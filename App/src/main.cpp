6ba60e753657268724cafc623b9a2156c660ddb9
Souradeep Banerjee <rishibanerjee1101@gmail.com>
2025-12-02 19:58:08 -0700

    Initial commit: Project skeleton and dependencies

---
+++ App/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// In the future, you will include "Engine.h" here
+
+int main() {
+    std::cout << "Initializing Clip Studio Clone Engine..." << std::endl;
+    
+    // Engine::Init(); 
+    
+    std::cout << "Engine Started. Press Enter to exit." << std::endl;
+    std::cin.get();
+    return 0;
+}
